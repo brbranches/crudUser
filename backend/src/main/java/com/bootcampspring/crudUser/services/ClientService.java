a0d5f244c413109e53c07cae63d7afd636b29766
BR.Branches <brunojramosr@gmail.com>
2021-06-14 22:30:03 -0300

    Criação do findById

--- backend/src/main/java/com/bootcampspring/crudUser/services/ClientService.java
+++ backend/src/main/java/com/bootcampspring/crudUser/services/ClientService.java
@@ -1,7 +1,7 @@
 package com.bootcampspring.crudUser.services;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,5 +23,14 @@ public class ClientService {
 		List<Client> list = repository.findAll();
 		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
 	}
+
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.get();
+		return new ClientDTO(entity);
+	}
+
+
 
 }
